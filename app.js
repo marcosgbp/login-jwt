2056b9d8e29426b80866e6fb052e06c9420d5604
Marcos Benítez Paredes <marcbenite.mb@gmail.com>
2025-02-09 16:20:23 -0300

    Configuración para la entrega de archivos estáticos desde el directorio public

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import express from "express";
 import { PORT } from "./config.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory));
 
+app.set("view engine", "hbs");
 
+console.log(__dirname);
 
 app.get("/", (req, res) => {
-    res.send("<h1>Home Page</h1>");
+    res.render("index");
 });
 
 
