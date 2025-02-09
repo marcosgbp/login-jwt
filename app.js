1799fccbba5fae4a9b30ab9e154106aaa45e1d9a
Marcos Benítez Paredes <marcbenite.mb@gmail.com>
2025-02-09 15:28:10 -0300

    Configuraciones iniciales

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { PORT } from "./config.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
