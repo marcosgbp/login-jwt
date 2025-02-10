78cba4f463a4bf798f5b31e80e983f1581dc3fb9
Marcos Benítez Paredes <marcbenite.mb@gmail.com>
2025-02-09 23:24:24 -0300

    Creación de la ruta para manipular los datos recibidos del frontend

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import router from "./routes/pages.js";
+import authRouter from "./routes/auth.js";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -11,12 +12,15 @@ const __dirname = dirname(__filename);
 const publicDirectory = path.join(__dirname, "./public");
 app.use(express.static(publicDirectory));
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.set("view engine", "hbs");
 
 console.log(__dirname);
 
 //Rutas
 app.use("/", router);
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
